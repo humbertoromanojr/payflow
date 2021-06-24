5258ccfb2ee5d20e21fc118ad97223d326b4d257
jrdev <astronomi@gmail.com>
2021-06-23 22:55:18 -0300

    Aligning mini logo

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -15,14 +16,30 @@ class _LoginPageState extends State<LoginPage> {
     final sizeScreen = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Stack(
-        children: [
-          Container(
-            width: sizeScreen.width,
-            height: sizeScreen.height,
-            color: AppColors.primary,
-          )
-        ],
+      body: Container(
+        width: sizeScreen.width,
+        height: sizeScreen.height,
+        child: Stack(
+          children: [
+            Container(
+              width: sizeScreen.width,
+              height: sizeScreen.height * 0.4,
+              color: AppColors.primary,
+            ),
+            Positioned(top: 40, left: 0, right: 0, child: Image.asset(AppImages.person, width: 208, height: 373)),
+            Positioned(
+              bottom: sizeScreen.height * 0.27,
+              left: 0, right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini)
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
