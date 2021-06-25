324765190cc9d1a8e733803b472115af397d991a
jrdev <astronomi@gmail.com>
2021-06-24 23:26:58 -0300

    Setting user model

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -13,7 +14,11 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user = UserModel(
+            name: response!.displayName!, 
+            photoURL: response.photoUrl,
+          );
+      authController.setUser(context, user);
     } catch (error) {
       authController.setUser(context, null);
       print(error);
--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
   // ignore: unused_field
   var _isAuthenticated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  // _user! <-- if logged
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
       _user = user;
       _isAuthenticated = true;
@@ -17,8 +18,26 @@ class AuthController {
       Navigator.restorablePushReplacementNamed(context, "/home");
     } else {
       _isAuthenticated = false;
-      
-       Navigator.restorablePushReplacementNamed(context, "/login");
+
+      Navigator.restorablePushReplacementNamed(context, "/login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+
+    if(instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      setUser(context, null);
     }
   }
 }
