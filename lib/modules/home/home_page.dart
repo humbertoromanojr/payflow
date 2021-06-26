83a04d673af5e29700a92e8de4b3658ad54b8d5f
jrdev <astronomi@gmail.com>
2021-06-26 14:03:49 -0300

    Update home_page

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,9 +8,69 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home")
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Hi, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "JR Dev",
+                          style: TextStyles.buttonBoldBackground)
+                    ]),
+              ),
+              subtitle: Text("keep your accounts up to date.",
+                  style: TextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
+      bottomNavigationBar: Container(
+          height: 90,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.home),
+                color: AppColors.primary,
+              ),
+              GestureDetector(
+                onTap: () {
+                  print("click-me!");
+                },
+                child: Container(
+                  height: 56,
+                  width: 56,
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child:
+                      Icon(Icons.add_box_outlined, color: AppColors.background),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.description_outlined),
+                color: AppColors.body,
+              ),
+            ],
+          )),
     );
   }
 }
