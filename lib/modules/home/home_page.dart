8a9189a841b31ac7288e347527db02ec7feff6a5
jrdev <astronomi@gmail.com>
2021-06-26 19:43:11 -0300

    Changing to Stateful and linked to home controller

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+import 'home_controller.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +52,17 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+      body: pages[controller.currentPage],
       bottomNavigationBar: Container(
           height: 90,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
                 icon: Icon(Icons.home),
                 color: AppColors.primary,
               ),
@@ -65,7 +82,10 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  controller.setPage(1);
+                  setState(() {});
+                },
                 icon: Icon(Icons.description_outlined),
                 color: AppColors.body,
               ),
