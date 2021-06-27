0767d167ce81421d505aa601a9670a8f9784b29a
jrdev <astronomi@gmail.com>
2021-06-26 23:20:56 -0300

    Linking set_label_buttons component and refactoring barcode_scanner_page

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
-import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
-import 'package:payflow/shared/widgets/label_button/label_button.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   BarcodeScannerPage({Key? key}) : super(key: key);
@@ -37,26 +36,13 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
           Expanded(child: Container(color: Colors.black,)),
           ],
         ),
-      bottomNavigationBar: Container(
-        height: 56,
-        child: Row(
-          children: [
-            Expanded(
-              child: LabelButton(
-                label: "Insert code from billet", 
-                onPressed: () {}
-              ),
-            ),
-            DividerVerticalWidget(),
-            Expanded(
-              child: LabelButton(
-                label: "Insert code billet from gallery", 
-                onPressed: () {}
-              ),
-            ),
-          ],
-        ),
-      )
+      bottomNavigationBar: 
+        SetLabelButtons(
+          primaryLabel: "Insert code from billet", 
+          primaryOnPressed: () {},
+          secondaryLabel: "Insert code billet from gallery", 
+          secondaryOnPressed: () {}
+      ),
     );
   }
 }
