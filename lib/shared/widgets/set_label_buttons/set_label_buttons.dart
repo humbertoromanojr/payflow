89812adc0cee4312748ce31d2e14740feef9b3f4
jrdev <astronomi@gmail.com>
2021-06-26 23:01:19 -0300

    Create set_label_buttons component

---
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SetLabelButtons extends StatelessWidget {
+  const SetLabelButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
