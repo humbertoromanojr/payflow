0ffd5390a3464e7f7c115aea3d1325bdbcadfb96
jrdev <astronomi@gmail.com>
2021-06-26 21:47:31 -0300

    Create barcode_scanner_page

---
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  BarcodeScannerPage({Key? key}) : super(key: key);
+
+  @override
+  _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
