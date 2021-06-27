84ed39b50a01444a220c484493105506d9519099
jrdev <astronomi@gmail.com>
2021-06-26 22:25:10 -0300

    Create label_button component

---
+++ lib/shared/widgets/label_button/label_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LabelButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const LabelButton({
+    Key? key, 
+    required this.label, 
+    required this.onPressed
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: onPressed, child: Text(label));
+  }
+}
