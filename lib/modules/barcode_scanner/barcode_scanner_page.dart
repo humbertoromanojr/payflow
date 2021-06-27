d01bf9431b45b8e60a7f7496445b0f05d7cd8ca2
jrdev <astronomi@gmail.com>
2021-06-26 22:24:32 -0300

    Updating barcode_scanner_page

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   BarcodeScannerPage({Key? key}) : super(key: key);
@@ -10,8 +12,27 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: null,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Scan the barcode of ticket.",
+          style: TextStyles.buttonBackground,
+        ),
+        leading: BackButton(
+          color: AppColors.background,
+        ),
+      ),
+      body: Column(children: [
+        Expanded(child: Container(color: Colors.black,)),
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.transparent,
+            )
+          ),
+        Expanded(child: Container(color: Colors.black,)),
+      ],)
     );
   }
 }
