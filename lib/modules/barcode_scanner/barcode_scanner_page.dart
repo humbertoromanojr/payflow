04cc2e07a8deb21be8ac9b6e05d2b0b6d24a3a0f
jrdev <astronomi@gmail.com>
2021-06-27 11:14:45 -0300

    Updating barcode_scanner_page

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
@@ -13,37 +14,55 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return RotatedBox(
-      quarterTurns: 1,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Text(
-            "Scan the barcode of billet.",
-            style: TextStyles.buttonBackground,
+    return BottomSheetWidget(
+        title: "Unable to identify the barcode.",
+        subtitle: "Try scanning again or enter your bank slip code.",
+        primaryLabel: "Escanear novamente",
+        primaryOnPressed: () {},
+        secondaryLabel: "Digitar código",
+        secondaryOnPressed: () {},
+      );
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text(
+              "Scan the barcode of billet.",
+              style: TextStyles.buttonBackground,
+            ),
+            centerTitle: true,
+            leading: BackButton(
+              color: AppColors.background,
+            ),
           ),
-          leading: BackButton(
-            color: AppColors.background,
-          ),
-        ),
-        body: Column(
-          children: [
-            Expanded(child: Container(color: Colors.black,)),
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.transparent,
-                )
-              ),
-            Expanded(child: Container(color: Colors.black,)),
+          body: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                color: Colors.black,
+              )),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.transparent,
+                  )),
+              Expanded(
+                  child: Container(
+                color: Colors.black,
+              )),
             ],
           ),
-        bottomNavigationBar: 
-          SetLabelButtons(
-            primaryLabel: "Insert code from billet", 
-            primaryOnPressed: () {},
-            secondaryLabel: "Insert code billet from gallery", 
-            secondaryOnPressed: () {}
+          bottomNavigationBar: SetLabelButtons(
+              primaryLabel: "Insert code from billet",
+              primaryOnPressed: () {},
+              secondaryLabel: "Insert code billet from gallery",
+              secondaryOnPressed: () {}),
         ),
       ),
     );
