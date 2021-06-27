d326ebbabe56ed40184fb1a80cd51913aa914958
jrdev <astronomi@gmail.com>
2021-06-27 15:09:45 -0300

    Route and Linking insert_boleto_page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
+import 'modules/insert_boleto/insert_boleto_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
 
@@ -29,6 +30,7 @@ class AppWidget extends StatelessWidget {
           "/home": (context) => HomePage(),
           "/login": (context) => LoginPage(),
           "/barcode_scanner": (context) => BarcodeScannerPage(),
+          "/insert_boleto": (context) => InsertBoletoPage(),
         });
   }
 }
