180282fb54ef734ae86da0f55039e79b82344cc3
jrdev <astronomi@gmail.com>
2021-06-27 11:14:12 -0300

    Fixing the device in portrait position

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:flutter/services.dart';
 
+import 'package:payflow/shared/themes/app_colors.dart';
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        primaryColor: AppColors.primary,
-      ),
-      initialRoute: "/splash",
-      routes: {
-        "/splash": (context) => SplashPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
-        "/barcode_scanner": (context) => BarcodeScannerPage(),
-      }
-    );
+        title: 'Pay Flow',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          primaryColor: AppColors.primary,
+        ),
+        initialRoute: "/splash",
+        routes: {
+          "/splash": (context) => SplashPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
+        });
   }
 }
