b0cdf6b99028b0df94afcae81680f7097300b3b0
jrdev <astronomi@gmail.com>
2021-06-27 23:53:19 -0300

    Convert statelessWidget for statefulWidget

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
-class InsertBoletoPage extends StatelessWidget {
+class InsertBoletoPage extends StatefulWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
 
+  @override
+  _InsertBoletoPageState createState() => _InsertBoletoPageState();
+}
+
+class _InsertBoletoPageState extends State<InsertBoletoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: Text("Insert Boleto Page"),
         backgroundColor: AppColors.background,
+        leading: BackButton(color: AppColors.input),
         elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 93, vertical: 24),
+              child: Text(
+                "Fill in the payment slip data",
+                style: TextStyles.titleBoldHeading,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            InputTextWidget(
+              label: "Name to billet",
+              icon: Icons.description_outlined,
+              onChanged: (value) {},
+            ),
+            InputTextWidget(
+              label: "Maturity",
+              icon: FontAwesomeIcons.timesCircle,
+              onChanged: (value) {},
+            ),
+            InputTextWidget(
+              label: "Value",
+              icon: FontAwesomeIcons.wallet,
+              onChanged: (value) {},
+            ),
+            InputTextWidget(
+              label: "Code",
+              icon: FontAwesomeIcons.barcode,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SetLabelButtons(
+        enableSecondaryColor: true,
+        primaryLabel: "Cancel",
+        primaryOnPressed: () {
+          Navigator.pop(context);
+        },
+        secondaryLabel: "Register",
+        secondaryOnPressed: () {},
       ),
     );
   }
