e4b8707743e3cf504726160ea57fdde2be2d1157
jrdev <astronomi@gmail.com>
2021-06-27 21:22:05 -0300

    Errors fix in module barcode_scanner

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_status.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
+
+import 'barcode_scanner_controller.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   BarcodeScannerPage({Key? key}) : super(key: key);
@@ -12,58 +15,107 @@ class BarcodeScannerPage extends StatefulWidget {
 }
 
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  final controller = BarcodeScannerController();
+
+  @override
+  void initState() {
+    controller.getAvailableCameras();
+    controller.statusNotifier.addListener(() {
+      if (controller.status.hasBarcode) {
+        Navigator.pushReplacementNamed(context, "/insert_boleto");
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomSheetWidget(
-        title: "Unable to identify the barcode.",
-        subtitle: "Try scanning again or enter your bank slip code.",
-        primaryLabel: "Escanear novamente",
-        primaryOnPressed: () {},
-        secondaryLabel: "Digitar código",
-        secondaryOnPressed: () {},
-      );
     return SafeArea(
       top: true,
       bottom: true,
       left: true,
       right: true,
-      child: RotatedBox(
-        quarterTurns: 1,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text(
-              "Scan the barcode of billet.",
-              style: TextStyles.buttonBackground,
-            ),
-            centerTitle: true,
-            leading: BackButton(
-              color: AppColors.background,
-            ),
+      child: Stack(
+        children: [
+          ValueListenableBuilder<BarcodeScannerStatus>(
+            valueListenable: controller.statusNotifier,
+            builder: (_, status, __) {
+              if (status.showCamera) {
+                return Container(
+                  child: controller.cameraController!.buildPreview(),
+                );
+              } else {
+                return Container();
+              }
+            },
           ),
-          body: Column(
-            children: [
-              Expanded(
-                  child: Container(
-                color: Colors.black,
-              )),
-              Expanded(
-                  flex: 2,
-                  child: Container(
-                    color: Colors.transparent,
+          RotatedBox(
+            quarterTurns: 1,
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                title: Text(
+                  "Scan the barcode of billet.",
+                  style: TextStyles.buttonBackground,
+                ),
+                centerTitle: true,
+                leading: BackButton(
+                  color: AppColors.background,
+                ),
+              ),
+              body: Column(
+                children: [
+                  Expanded(
+                      child: Container(
+                    color: Colors.black,
                   )),
-              Expanded(
-                  child: Container(
-                color: Colors.black,
-              )),
-            ],
+                  Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.transparent,
+                      )),
+                  Expanded(
+                      child: Container(
+                    color: Colors.black,
+                  )),
+                ],
+              ),
+              bottomNavigationBar: SetLabelButtons(
+                primaryLabel: "Insert code from billet",
+                primaryOnPressed: () {
+                  Navigator.pushReplacementNamed(context, "/inserir_boleto");
+                },
+                secondaryLabel: "Insert code billet from gallery",
+                secondaryOnPressed: () {}),
+            ),
           ),
-          bottomNavigationBar: SetLabelButtons(
-              primaryLabel: "Insert code from billet",
-              primaryOnPressed: () {},
-              secondaryLabel: "Insert code billet from gallery",
-              secondaryOnPressed: () {}),
-        ),
+          ValueListenableBuilder<BarcodeScannerStatus>(
+            valueListenable: controller.statusNotifier,
+            builder: (_, status, __) {
+              if (status.hasError) {
+                return BottomSheetWidget(
+                  title: "Unable to identify the barcode.",
+                  subtitle: "Try scanning again or enter your bank slip code.",
+                  primaryLabel: "Escanear novamente",
+                  primaryOnPressed: () {
+                    controller.scanWithCamera();
+                  },
+                  secondaryLabel: "Digitar código",
+                  secondaryOnPressed: () {},
+                );
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/modules/barcode_scanner/barcode_scanner_status.dart
+++ lib/modules/barcode_scanner/barcode_scanner_status.dart
@@ -1,35 +1,49 @@
 import 'package:camera/camera.dart';
 
 class BarcodeScannerStatus {
-  final bool isCameraAvaliable;
+  final bool isAvailable;
   final String error;
   final String barcode;
-
-  CameraController? cameraController;
+  final bool stopScanner;
 
   // constructor
   BarcodeScannerStatus({
-    this.isCameraAvaliable = false,
-    this.cameraController,
+    this.isAvailable = false,
+    this.stopScanner = false,
     this.error = "",
     this.barcode = "",
   });
 
-  factory BarcodeScannerStatus.available(CameraController controller) =>
+  factory BarcodeScannerStatus.available() =>
       BarcodeScannerStatus(
-        isCameraAvaliable: true,
-        cameraController: controller,
+        isAvailable: true,
+        stopScanner: false
       );
   
   factory BarcodeScannerStatus.error(String message) =>
-    BarcodeScannerStatus(error: message);
+    BarcodeScannerStatus(error: message, stopScanner: true);
   
   factory BarcodeScannerStatus.barcode(String barcode) =>
-  BarcodeScannerStatus(barcode: barcode);
+  BarcodeScannerStatus(barcode: barcode, stopScanner: true);
 
-  bool get showCamera => isCameraAvaliable && error.isEmpty;
+  bool get showCamera => isAvailable && error.isEmpty;
 
   bool get hasError => error.isNotEmpty;
 
   bool get hasBarcode => barcode.isNotEmpty;
+
+   BarcodeScannerStatus copyWith({
+    bool? isAvailable,
+    String? error,
+    String? barcode,
+    bool? stopScanner,
+    CameraController? cameraController,
+  }) {
+    return BarcodeScannerStatus(
+      isAvailable: isAvailable ?? this.isAvailable,
+      error: error ?? this.error,
+      barcode: barcode ?? this.barcode,
+      stopScanner: stopScanner ?? this.stopScanner,
+    );
+  }
 }
