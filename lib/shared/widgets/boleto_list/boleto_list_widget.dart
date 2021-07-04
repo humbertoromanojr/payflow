e2b37287ea70b4f454d374d3c8b1db375dac51f1
jrdev <astronomi@gmail.com>
2021-07-04 14:00:30 -0300

    Create boleto_list_widget component

---
+++ lib/shared/widgets/boleto_list/boleto_list_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BoletoListWidget extends StatefulWidget {
+  BoletoListWidget({Key? key}) : super(key: key);
+
+  @override
+  _BoletoListWidgetState createState() => _BoletoListWidgetState();
+}
+
+class _BoletoListWidgetState extends State<BoletoListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
