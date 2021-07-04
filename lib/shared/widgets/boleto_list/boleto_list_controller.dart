7d72862d88866e0bc3b326a5ffdd556dd06a8a4d
jrdev <astronomi@gmail.com>
2021-07-04 14:55:37 -0300

    Setting getBillets

--- lib/shared/widgets/boleto_list/boleto_list_controller.dart
+++ lib/shared/widgets/boleto_list/boleto_list_controller.dart
@@ -1,5 +1,20 @@
 import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BoletoListController {
   List<BoletoModel> billets = <BoletoModel>[];
+
+  BoletoListController() {
+    getBillets();
+  }
+
+  Future<void> getBillets() async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+      final response = instance.getStringList("billets") ?? <String>[];
+      billets = response.map((e) => BoletoModel.fromJson(e)).toList();
+    } catch (e) {
+      billets = <BoletoModel>[];
+    }
+  }
 }
