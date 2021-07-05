0a9399af9d8baf356a7ed245fd25c3c481b5b47a
jrdev <astronomi@gmail.com>
2021-07-04 23:15:33 -0300

    Convert stateless for stateful page

--- lib/modules/my_billets/my_billets_page.dart
+++ lib/modules/my_billets/my_billets_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MyBilletsPage extends StatelessWidget {
+class MyBilletsPage extends StatefulWidget {
   const MyBilletsPage({Key? key}) : super(key: key);
 
+  @override
+  _MyBilletsPageState createState() => _MyBilletsPageState();
+}
+
+class _MyBilletsPageState extends State<MyBilletsPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
