61d91b75c7451516a921f3d7fa44031af63cfdc0
jrdev <astronomi@gmail.com>
2021-07-05 00:08:59 -0300

    Setting aligment

--- lib/shared/widgets/boleto_info/boleto_info_widget.dart
+++ lib/shared/widgets/boleto_info/boleto_info_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class BoletoInfoWidget extends StatelessWidget {
   final int size;
@@ -11,7 +14,43 @@ class BoletoInfoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: null,
+      decoration: BoxDecoration(
+        color: AppColors.secondary,
+        borderRadius: BorderRadius.circular(5)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              AppImages.logomini,
+              color: AppColors.background,
+              width: 56,
+              height: 34,
+            ),
+            Container(
+              width: 1,
+              height: 32,
+              color: AppColors.background,
+            ),
+            Text.rich(TextSpan(
+              text: "You have ",
+              style: TextStyles.captionBackground,
+              children: [
+                TextSpan(
+                  text: "$size billets\n",
+                  style: TextStyles.captionBoldBackground,
+                ),
+                TextSpan(
+                  text: "registered to pay ",
+                  style: TextStyles.captionBackground,
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
