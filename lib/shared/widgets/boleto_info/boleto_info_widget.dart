591d61937d7eb64ed2cfaa2a95556f266c41bb9a
jrdev <astronomi@gmail.com>
2021-07-04 23:46:40 -0300

    Create boleto_info_widget component

---
+++ lib/shared/widgets/boleto_info/boleto_info_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BoletoInfoWidget extends StatelessWidget {
+  final int size;
+
+  const BoletoInfoWidget({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
