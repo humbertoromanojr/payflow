2b396e89441d1664f2ab90d0d8249d829010112a
jrdev <astronomi@gmail.com>
2021-07-08 21:08:53 -0300

    Solving overflow

--- lib/modules/my_billets/my_billets_page.dart
+++ lib/modules/my_billets/my_billets_page.dart
@@ -18,50 +18,52 @@ class _MyBilletsPageState extends State<MyBilletsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Container(
-              height: 40, 
-              width: double.maxFinite,
-              color: AppColors.primary,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: ValueListenableBuilder<List<BoletoModel>>(
-                valueListenable: controller.boletosNotifier,
-                builder: (_, billets, __) => BoletoInfoWidget(size: billets.length),
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
-          child: Row(
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
             children: [
-              Text(
-                "My Billets", 
-                style: TextStyles.titleBoldHeading
+              Container(
+                height: 40, 
+                width: double.maxFinite,
+                color: AppColors.primary,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ValueListenableBuilder<List<BoletoModel>>(
+                  valueListenable: controller.boletosNotifier,
+                  builder: (_, billets, __) => BoletoInfoWidget(size: billets.length),
+                ),
               ),
             ],
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-          child: Divider(
-            thickness: 1,
-            height: 1,
-            color: AppColors.stroke,
+          Padding(
+            padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: Row(
+              children: [
+                Text(
+                  "My Billets", 
+                  style: TextStyles.titleBoldHeading
+                ),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: BoletoListWidget(
-            controller: controller,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+            child: Divider(
+              thickness: 1,
+              height: 1,
+              color: AppColors.stroke,
+            ),
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: BoletoListWidget(
+              controller: controller,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
